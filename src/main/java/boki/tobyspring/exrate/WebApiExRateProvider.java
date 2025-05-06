842dd6cda513c152b948abb9c7e545cda1270d08
boki <lsb530@naver.com>
2025-05-07 01:04:54 +0900

    ApiTemplate 분리

--- src/main/java/boki/tobyspring/exrate/WebApiExRateProvider.java
+++ src/main/java/boki/tobyspring/exrate/WebApiExRateProvider.java
@@ -1,53 +1,21 @@
 package boki.tobyspring.exrate;
 
-import boki.tobyspring.api.ApiExecutor;
+import boki.tobyspring.api.ApiClientApiExecutor;
+import boki.tobyspring.api.ApiTemplate;
 import boki.tobyspring.api.ExApiExRateExtractor;
-import boki.tobyspring.api.ExRateExtractor;
-import boki.tobyspring.api.SimpleApiExecutor;
 import boki.tobyspring.payment.ExRateProvider;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
-// @Component
 public class WebApiExRateProvider implements ExRateProvider {
+
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        // return runApiForExRate(url, new SimpleApiExecutor(), new ExApiExRateExtractor());
-        return runApiForExRate(url, new SimpleApiExecutor(), response -> {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-            return data.rates().get("KRW");
-        });
-    }
-
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return apiTemplate.getExRate(url, new ApiClientApiExecutor(), new ExApiExRateExtractor());
     }
 
 }
