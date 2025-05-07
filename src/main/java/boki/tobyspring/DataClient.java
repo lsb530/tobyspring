8c6ddecffec13920298308a48159383843569f11
boki <lsb530@naver.com>
2025-05-08 04:16:14 +0900

    DataAccessException 예외 추상화

--- src/main/java/boki/tobyspring/DataClient.java
+++ src/main/java/boki/tobyspring/DataClient.java
@@ -4,6 +4,7 @@ import boki.tobyspring.data.OrderRepository;
 import boki.tobyspring.order.Order;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
@@ -18,20 +19,25 @@ public class DataClient {
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
         JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        new TransactionTemplate(transactionManager).execute(new TransactionCallback<Order>() {
-            @Override
-            public Order doInTransaction(TransactionStatus status) {
-                Order order = new Order("100", BigDecimal.TEN);
-                repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute(new TransactionCallback<Order>() {
+                @Override
+                public Order doInTransaction(TransactionStatus status) {
+                    Order order = new Order("100", BigDecimal.TEN);
+                    repository.save(order);
 
-                System.out.println(order);
+                    System.out.println(order);
 
-                // Order order2 = new Order("100", BigDecimal.ONE);
-                // repository.save(order2);
+                    Order order2 = new Order("100", BigDecimal.ONE);
+                    repository.save(order2);
 
-                return null;
-            }
-        });
+                    return null;
+                }
+            });
+        }
+        catch (DataIntegrityViolationException e) {
+            System.out.println("주문번호 중복 복구 작업");
+        }
 
 
     }
