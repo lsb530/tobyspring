c5c8e027da5391d5ac488159af1a138ec2c7a4b8
boki <lsb530@naver.com>
2025-05-08 04:16:51 +0900

    DataAccessException 예외 추상화2 - 람다로

--- src/main/java/boki/tobyspring/DataClient.java
+++ src/main/java/boki/tobyspring/DataClient.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
@@ -20,22 +18,18 @@ public class DataClient {
         JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
         try {
-            new TransactionTemplate(transactionManager).execute(new TransactionCallback<Order>() {
-                @Override
-                public Order doInTransaction(TransactionStatus status) {
-                    Order order = new Order("100", BigDecimal.TEN);
-                    repository.save(order);
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-                    System.out.println(order);
+                System.out.println(order);
 
-                    Order order2 = new Order("100", BigDecimal.ONE);
-                    repository.save(order2);
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
 
-                    return null;
-                }
+                return null;
             });
-        }
-        catch (DataIntegrityViolationException e) {
+        } catch (DataIntegrityViolationException e) {
             System.out.println("주문번호 중복 복구 작업");
         }
 
