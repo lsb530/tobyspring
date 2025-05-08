e0f3b97eb1104f1b895bca35412568815102264c
boki <lsb530@naver.com>
2025-05-08 16:24:57 +0900

    트랜잭션 테스트 - 하나의 트랜잭션으로 묶어, 전체 실패하게 만들어서 성공

--- src/main/java/boki/tobyspring/order/OrderService.java
+++ src/main/java/boki/tobyspring/order/OrderService.java
@@ -2,6 +2,7 @@ package boki.tobyspring.order;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -25,7 +26,9 @@ public class OrderService {
     }
 
     public List<Order> createOrders(List<OrderReq> reqs) {
-        return reqs.stream().map(req -> createOrder(req.no(), req.total())).toList();
+        return new TransactionTemplate(transactionManager).execute(status ->
+            reqs.stream().map(req -> createOrder(req.no(), req.total())).toList()
+        );
     }
 
 }
